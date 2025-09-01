0aeb96f93e4c1ea59ebf87f1b499d22a63464979
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2025-09-01 17:33:56 -0300

    criando o arquivo da modal de detalhe demanda

---
+++ front-smsm/src/app/modals/detalhe-demanda-modal/detalhe-demanda-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-demanda-modal',
+  imports: [],
+  templateUrl: './detalhe-demanda-modal.html',
+  styleUrl: './detalhe-demanda-modal.scss'
+})
+export class DetalheDemandaModal {
+
+}
