a6a82404a3b382ff92324c13d84a135a3d365c75
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2025-09-14 01:23:51 -0300

    criando service genérica

---
+++ front-smsm/src/app/services/generic-service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenericService {
+  constructor(
+    private router: Router
+  ) {}
+
+  public irPara(rota: string[]) {
+    this.router.navigate(rota);
+  }
+
+  public buscarData(cidade: string) {
+    const agora = new Date();
+    const dia = agora.getDate();
+    const mes = agora.toLocaleString("pt-BR", { month: "long" });
+    const ano = agora.getFullYear();
+
+    return `${cidade}, ${dia} de ${mes} de ${ano}`;
+  }
+}
