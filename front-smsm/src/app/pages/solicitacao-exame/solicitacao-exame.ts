c6be172bf2750639d3802d863b7937af51883799
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2025-09-14 17:21:21 -0300

    desenvolvendo tela de solicitações

--- front-smsm/src/app/pages/solicitacao-exame/solicitacao-exame.ts
+++ front-smsm/src/app/pages/solicitacao-exame/solicitacao-exame.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { GenericService } from '../../services/generic-service';
 
 @Component({
   selector: 'app-solicitacao-exame',
-  imports: [],
+  imports: [MatSidenavModule],
   templateUrl: './solicitacao-exame.html',
   styleUrl: './solicitacao-exame.scss'
 })
 export class SolicitacaoExame {
+  public data: string = '';
 
+  constructor(
+    private genericService: GenericService
+  ) {}
+
+  ngOnInit() {
+    this.buscarData();
+  }
+
+  public irPara(rota: string[]) {
+    this.genericService.irPara(rota);
+  }
+
+  public buscarData() {
+    this.data = this.genericService.buscarData('Marília');
+  }
 }
